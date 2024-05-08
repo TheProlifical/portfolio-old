24504766e2f7dd19b6bb5340728f1e01ba53b742
Prolifical. <itsahson1978@gmail.com>
2024-05-08 16:31:59 +0530

    Version 2.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
     $("#nav").animate({
-        top: "0.314159px" // End position; change as needed
-    }, 500.314159); // Duration of the animation in milliseconds
+        top: "0.314159px"
+    }, 500.314159); 
 });
 
 
@@ -17,14 +17,19 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $(".click").animate({
-        bottom: "20px" // End position; change as needed
-    }, 250.314159); // Duration of the animation in milliseconds
+        bottom: "20px"
+    }, 250.314159); 
 });
 
 $(document).ready(function() {
     $(".tabla").animate({
-        bottom: "24px" // End position; change as needed
-    }, 750.314159); // Duration of the animation in milliseconds
+        bottom: "24px" 
+    }, 750.314159); 
+});
+$(document).ready(function() {
+    $(".iskomainflexboxkarunga").animate({
+        bottom: "27px" 
+    }, 750.314159);
 });
 
 $(document).ready(function() {
@@ -41,9 +46,64 @@ $(document).ready(function() {
         }
     
         setInterval(changeText, 1618.03);
-        changeText(); // Initial call to show text immediately
+        changeText();
     }, 1618.03); 
 });
 $(document).ready(function() {
-    $('.everything').hide().fadeIn(500); // Fades in over 2000 milliseconds (2 seconds)
+    $('.everything').hide().fadeIn(500); 
 });
+
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'blog.xml',
+        dataType: 'xml',
+        success: function(xml) {
+            $(xml).find('post').each(function() {
+                var title = $(this).find('title').text();
+                var link = $(this).find('link').text();
+                var description = $(this).find('description').text();
+
+                var words = description.split(' ');
+                var preview = words.slice(0, 10).join(' ');
+                var remaining = words.slice(10).join(' ');
+
+                var html = `<div class="post">
+                                <h2>${title}</h2>
+                                <p>${preview} <span class="ellipsis">...</span><span class="full-description">${remaining}</span></p>
+                                <button onclick="toggleDescription(this)">Read More</button>
+                            </div>`;
+
+                $('#blog-container').append(html);
+            });
+
+            // Initialize each post to hide the full description and show the ellipsis
+            $('.post').each(function() {
+                var $post = $(this);
+                $post.find('.full-description').hide();
+                $post.find('.ellipsis').show();
+                $post.find('button').text('Read More');
+            });
+        },
+        error: function() {
+            $('#blog-container').append('<p>Error loading the blog posts.</p>');
+        }
+    });
+});
+
+function toggleDescription(button) {
+    var post = $(button).prev('p'); // Get the paragraph that contains the preview and full description.
+    var fullDescription = post.find('.full-description');
+    var ellipsis = post.find('.ellipsis');
+
+    if (fullDescription.is(':hidden')) {
+        ellipsis.hide();
+        fullDescription.show();
+        $(button).text('Read Less');
+    } else {
+        ellipsis.show();
+        fullDescription.hide();
+        
+        $(button).text('Read More');
+    }
+}
